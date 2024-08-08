19060dc18b0b10f9431c0815d723b1af2209e4ee
Vinícius <viniciuspadilhavieira@hotmail.com>
2024-08-08 18:19:58 -0400

    feat: código pronto

---
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,6 @@
+public class ParametrosInvalidosException extends Exception {
+    // Construtor que aceita uma mensagem
+    public ParametrosInvalidosException(String mensagem) {
+        super(mensagem);
+    }
+}
